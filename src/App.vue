41803ee9a503f079f69e810c1a687eb97e5503f0
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-16 11:42:51 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import LaunchesVue from './components/LaunchesVue.vue';
+import CapsulesVue from './components/CapsulesVue.vue';
+</script>
+
+<template>
+  <div>
+    <launches-vue></launches-vue>
+    <capsules-vue></capsules-vue>
+  </div>
+</template>
---
+++ src/components/LaunchesVue.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CapsulesVue from './CapsulesVue.vue';
+</script>
+
+<template>
+  <div>
+    <capsules-vue></capsules-vue>
+  </div>
+</template>
