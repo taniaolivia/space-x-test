00cf288abaf78f8e7fafda1f1b07c8084b8dcf2e
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-17 00:05:08 +0100

    add router and axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '../public/index.css'
 import axios from 'axios'
+import router from './router'
 
 const app = createApp(App);
 
 app.provide("api", axios.create({
     baseURL: "https://api.spacexdata.com/v4"
 }));
+
+app.provide("route", )
+
+app.use(router);
 
 app.mount('#app');
