13848c2d4d195db911d8c0fd173789ee65618343
Tania Olivia <tania.olivia69@yahoo.com>
2023-01-17 00:09:29 +0100

    add footer

---
+++ src/components/FooterVue.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <div class="bg-gray-600 w-screen py-2 text-white flex flex-row justify-center sticky bottom-0">
+    <span class="font-bold">@Space X - Tania OLIVIA</span>
+  </div>
+</template>
